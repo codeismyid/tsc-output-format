63b08f19dbdaf8c8aba39e98370cbf41083615d0
id <codeismyid@gmail.com>
2025-04-06 03:37:00 +0700

    fix: inefficient regex from codeql scan

--- src/parsers/default-parser.ts
+++ src/parsers/default-parser.ts
@@ -14,7 +14,7 @@ const KEYS = [
  * Default `Parser` instance.
  */
 export const defaultParser = new Parser(
-  /^(?:(?:(.*?)[:(](\d+)[:,](\d+)[)]? ?[:-] ?)|(?:error ?))(?:error ?)?(TS\d+)?(?:(?:: )|(?: - )|(?: ))(.*(?:\r?\n {2,}.*)*)(?:(?:\r?\n){2,}((?:\d+\s+.*\r?\n\s+~+(?:\r?\n)?)*))?$/gm,
+  /^(?:(?:(.*?)[:(](\d+)[:,](\d+)[)]? ?[:-] ?)|(?:error ?))(?:error ?)?(TS\d+)?(?:(?:: )|(?: - )|(?: ))(.*(?:\r?\n {2,}.*)*)$(?:(?:\r?\n){2,}^((?:\d+\s+\S.*\r?\n^\s+~+$(?:\r?\n){0,1})*)){0,1}$/gm,
   KEYS,
   (input) => {
     // biome-ignore lint/suspicious/noControlCharactersInRegex: needed for removing colored text
